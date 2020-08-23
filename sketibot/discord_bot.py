7d9680cff6bb51040dcdcff8865a7706b8d14e59
Erik <eknyquist@gmail.com>
2020-08-23 10:49:52 -0700

    Working on it...

--- sketibot/discord_bot.py
+++ sketibot/discord_bot.py
@@ -1,6 +1,12 @@
 import os
-
 import discord
+import threading
+
+# The guild ID of the only guild this bot will send messages to
+GUILD_ID = 626753649602986024
+
+CHANNEL_NAME = 'live-strimmers'
+
 
 class MessageResponse(object):
     def __init__(self, response_data, channel=None, member=None):
@@ -9,10 +15,30 @@ class MessageResponse(object):
         self.response_data = response_data
 
 class DiscordBot(object):
-    def __init__(self, token, server):
+    def __init__(self, token, guild_id, channel_name):
         self.token = token
-        self.server = server
+        self.guild_id = guild_id
+        self.channel_name = channel_name
         self.client = discord.Client()
+
+        self.thread = threading.Thread(target=self.thread_task)
+        self.thread.start()
+
+        self.channel = None
+
+        @self.client.event
+        async def on_guild_available(guild):
+            if self.guild_id == guild.id:
+                for c in guild.text_channels:
+                    print(c.name)
+                    if c.name == self.channel_name:
+                        self.channel = c
+                        break
+
+            if self.channel is None:
+                raise RuntimeError("Unable to find channel '%s'" % CHANNEL_NAME)
+
+            await self.channel.send("Testing!")
 
         @self.client.event
         async def on_connect():
@@ -42,11 +68,24 @@ class DiscordBot(object):
     def run(self):
         self.client.run(self.token)
 
+    def thread_task(self):
+        pass
+
+    def send_message(self, channel_name, message):
+        pass
+
     def on_connect(self):
-        raise NotImplementedError()
+        pass
 
     def on_member_join(self, member):
-        raise NotImplementedError()
+        pass
 
     def on_message(self, message):
-        raise NotImplementedError()
+        pass
+
+def main():
+    bot = DiscordBot(os.environ['SKETIBOT_DISCORD_TOKEN'], GUILD_ID, CHANNEL_NAME)
+    bot.run()
+
+if __name__ == "__main__":
+    main()
