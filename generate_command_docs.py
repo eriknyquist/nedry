ba0bf0bf9e3ccdcbfbaa835c7163cbf6af50ea9c
Erik K. Nyquist <eknyquist@gmail.com>
2022-12-10 04:13:20 +0000

    Updating generate_command_docs.py

--- generate_command_docs.py
+++ generate_command_docs.py
@@ -1,9 +1,24 @@
+from unittest import mock
+from nedry.discord_bot import DiscordBot
+from nedry.plugin import PluginModuleManager
 from nedry.command_processor import nedry_command_list
 from nedry.builtin_plugins import builtin_plugin_modules
 
 CMD_TITLE_UNDERLINE_CHAR = '-'
 
-for cmd in nedry_command_list:
+with mock.patch('nedry.discord_bot.discord') as mock_discord:
+    bot = DiscordBot(mock.MagicMock(), mock.MagicMock())
+    plugin_manager = PluginModuleManager(bot, [])
+
+# Load built-in plugins
+names = []
+for plugin in builtin_plugin_modules:
+    plugin_manager.add_plugin_class(plugin)
+    names.append(plugin.plugin_name)
+
+plugin_manager.enable_plugins(names)
+
+for cmd in bot.cmdprocessor.cmds.values():
     # Print title
     title = "Command ``%s``" % cmd.word
     print(title)
